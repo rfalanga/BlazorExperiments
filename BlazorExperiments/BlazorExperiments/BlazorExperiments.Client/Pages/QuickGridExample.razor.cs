6009cb04b367730be86666f1ba845a10d0f1ed9d
Rod Falanga <rfalanga@users.noreply.github.com>
2026-02-07 08:22:41 -0700

    The QuickGrid has to reference anIQueryable object, not
    a List!! #4

--- BlazorExperiments/BlazorExperiments/BlazorExperiments.Client/Pages/QuickGridExample.razor.cs
+++ BlazorExperiments/BlazorExperiments/BlazorExperiments.Client/Pages/QuickGridExample.razor.cs
@@ -4,11 +4,11 @@ namespace BlazorExperiments.Client.Pages;
 
 public partial class QuickGridExample
 {
-    private List<Person> Family= new List<Person>
+    private IQueryable<Person> Family= new List<Person>
     {
         new Person { Id = 1, FirstName = "John", LastName = "Doe", Age = 40 },
         new Person { Id = 2, FirstName = "Jane", LastName = "Doe", Age = 39 },
         new Person { Id = 3, FirstName = "Sam", LastName = "Doe", Age = 10 },
         new Person { Id = 4, FirstName = "Emily", LastName = "Doe", Age = 8 }
-    };
+    }.AsQueryable();
 }
