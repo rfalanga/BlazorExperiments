b231a394605a31bfae9f82a399a308a59ea6740f
Rod Falanga <rfalanga@users.noreply.github.com>
2026-02-07 08:17:31 -0700

    Trying to get this to work #4

--- BlazorExperiments/BlazorExperiments/BlazorExperiments.Client/Pages/QuickGridExample.razor.cs
+++ BlazorExperiments/BlazorExperiments/BlazorExperiments.Client/Pages/QuickGridExample.razor.cs
@@ -8,7 +8,7 @@ public partial class QuickGridExample
     {
         new Person { Id = 1, FirstName = "John", LastName = "Doe", Age = 40 },
         new Person { Id = 2, FirstName = "Jane", LastName = "Doe", Age = 39 },
-        new Person { Id = 3, FirstName = "Sam", LastName = "Smith", Age = 10 },
-        new Person { Id = 4, FirstName = "Emily", LastName = "Johnson", Age = 8 },
+        new Person { Id = 3, FirstName = "Sam", LastName = "Doe", Age = 10 },
+        new Person { Id = 4, FirstName = "Emily", LastName = "Doe", Age = 8 }
     };
 }
