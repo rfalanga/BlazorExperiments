20b17850ef2a2541b1f6b5b395766e0660ac4841
Rod Falanga <rfalanga@users.noreply.github.com>
2026-02-07 07:19:25 -0700

    I forgot to add an Id to the Person class.
    Also added a List<Person> named Family. #4

---
+++ BlazorExperiments/BlazorExperiments/BlazorExperiments.Client/Pages/QuickGridExample.razor.cs
@@ -0,0 +1,14 @@
+﻿using BlazorExperiments.Client.Models;
+
+namespace BlazorExperiments.Client.Pages;
+
+public partial class QuickGridExample
+{
+    private List<Person> Family= new List<Person>
+    {
+        new Person { Id = 1, FirstName = "John", LastName = "Doe", Age = 40 },
+        new Person { Id = 2, FirstName = "Jane", LastName = "Doe", Age = 39 },
+        new Person { Id = 3, FirstName = "Sam", LastName = "Smith", Age = 10 },
+        new Person { Id = 4, FirstName = "Emily", LastName = "Johnson", Age = 8 },
+    };
+}
